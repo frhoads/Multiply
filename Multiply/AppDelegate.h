bc663fe76f5a15dab6538ced0f89297ba2dfc7d8
Kagan Riedel <kaganriedel@gmail.com>
2014-01-07 15:59:28 -0600

    first commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by Kagan Riedel on 1/7/14.
+//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multiply/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Multiply
+//
+//  Created by Kagan Riedel on 1/7/14.
+//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
